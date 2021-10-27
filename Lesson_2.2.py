e341c94ac3fbb5d5d09e0a87dc3bf1574a409079
Aleksei202 <nevzorov74@gmail.com>
2021-10-27 16:46:27 -0400

    Create Lesson_2.2.py

---
+++ Lesson_2.2.py
@@ -0,0 +1,12 @@
+my_sting = "Test Automation"
+word_test = my_sting[:4]
+print(word_test)
+
+my_sting = "Test Automation"
+second_word = my_sting[5:]
+print(second_word)
+
+my_list = [10, 50, 20, 25, 30]
+sliced_list = my_list[1:3]
+print(sliced_list)
+
