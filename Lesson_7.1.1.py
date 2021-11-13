eaa0b9ae040f309d93edeed95d288b8e39556b6b
Aleksei202 <nevzorov74@gmail.com>
2021-11-13 17:48:58 -0500

    Update Lesson_7.1.1.py

--- Lesson_7.1.1.py
+++ Lesson_7.1.1.py
@@ -7,10 +7,14 @@ result = True
 
 for i in range(0, 7):
     number = int(input('Enter number from 0 to 50\n'))
-    if number == dice:
+    if number > dice:
+        print('The value is too high')
+    elif number < dice:
+        print('The value is too low')
+    elif number == dice:
         result = False
-        print('You win!')
+        print('You guessed correctly!')
         break
 
 if result:
-    print(f'You fail it was {dice}')
+    print(f'You fail, it was {dice}')
