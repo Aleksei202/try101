79eb814de706ea14feb91a957bf973164a0fb382
Aleksei202 <nevzorov74@gmail.com>
2021-11-16 22:46:43 -0500

    Create Lesson_7.3.2.py

---
+++ Lesson_7.3.2.py
@@ -0,0 +1,11 @@
+def compute_patterns(pattern='new pattern'):
+    inputs = [pattern]
+    patterns = ['a list based on'] + inputs
+
+    return patterns
+
+
+print(compute_patterns())
+print(compute_patterns())
+print(compute_patterns('1'))
+print(compute_patterns('2'))
